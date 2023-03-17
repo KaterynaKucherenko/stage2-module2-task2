900e312c1dbb31e1692a163dde55e00360f45f86
KaterynaKucherenko <estorskaya@gmail.com>
2023-03-18 01:13:51 +0300

    done

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,15 @@
 package com.example.listener;
 
-public class ContextListener {
-    //write your code here!
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.time.LocalDateTime;
+
+@WebListener
+public class ContextListener implements HttpSessionListener {
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        se.getSession().setAttribute("servletTimeInit", LocalDateTime.now());
+    }
 }
+
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,31 @@
 package com.example.servlet;
 
-public class LoginServlet {
+import com.example.Users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet ("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       if(req.getSession().getAttribute("user") !=null){
+           resp.sendRedirect("/login.jsp");}
+       else{
+           resp.sendRedirect("/user/hello.jsp");
+       }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      if((Users.getInstance().getUsers().contains(req.getParameter("login"))) && (req.getParameter("password")!=null) && !(req.getParameter("password").trim().isEmpty())){
+          resp.sendRedirect("/user/hello.jsp");}
+     else{
+         req.getRequestDispatcher(" /login.jsp").forward(req, resp);
+      }
     //write your code here!
-}
+}}
